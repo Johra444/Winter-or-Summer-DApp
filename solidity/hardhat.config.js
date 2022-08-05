b28550419531db5a719e1f637b9a81e609a65c3b
Johra <52466288+Johra444@users.noreply.github.com>
2022-08-05 11:42:29 +0200

    Update hardhat.config.js

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
   solidity: "0.8.13",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/kkDRfmaW7BQ2cQJ_0-Dfz29gXJeasTl7",
-      accounts: [""]
+      url: "api",
+      accounts: ["privatekey"]
     },
   },
 };
